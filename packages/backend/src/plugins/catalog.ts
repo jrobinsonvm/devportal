ea1248c5ac8c8bb80288690a1e520b861c7fe44c
James <jrobinson001@my.wilmu.edu>
2022-10-13 14:17:52 -0400

    update with aws

---
+++ packages/backend/src/plugins/catalog.ts
@@ -0,0 +1,22 @@
+import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
+import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
+import { Router } from 'express';
+import { PluginEnvironment } from '../types';
+
+//new AwsEKSClusterProcessor({
+//  credentialsFactory: () => new AWS.CredentialProviderChain().resolvePromise(),
+//}),
+
+export default async function createPlugin(
+  env: PluginEnvironment,
+): Promise<Router> {
+  const builder = await CatalogBuilder.create(env);
+  builder.addProcessor(new ScaffolderEntitiesProcessor());
+  const { processingEngine, router } = await builder.build();
+  await processingEngine.start();
+  return router;
+//  new AwsEKSClusterProcessor({
+ //  credentialsFactory: () => new AWS.CredentialProviderChain().resolvePromise(),
+  //}),
+
+}
